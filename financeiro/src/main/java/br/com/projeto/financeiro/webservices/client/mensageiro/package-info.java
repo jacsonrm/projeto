22097c8b4cb77ef792e24ab8b62640bdbda6baad
Jacson <jacsonrm@hotmail.com>
2018-08-17 14:15:24 -0300

    atualização

--- financeiro/src/main/java/br/com/projeto/financeiro/webservices/client/mensageiro/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://impl.mensagem.webservices.mensageiro.tjpr.gov/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package br.jus.tjpr.biblioteca.webservices.client.mensageiro;
